24de039c8b6d51634999c15100978976c8143e1b
Joseph Sherman <shermanator2018@gmail.com>
2021-02-27 18:04:22 -0600

    moves fleet to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -111,6 +112,10 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet"""
+        self.aliens.update()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
