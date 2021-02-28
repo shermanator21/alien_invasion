ce5998107eab86a369ead22bde10ea364a09c2ef
Joseph Sherman <shermanator2018@gmail.com>
2021-02-27 23:13:37 -0600

    score multiples working

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,6 +76,7 @@ class AlienInvasion:
             # Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -134,7 +135,8 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points + len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
